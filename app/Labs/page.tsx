7ce4a4aeee57e1b008237a551dd0236a994b7e99
Tori <tpostler87@gmail.com>
2025-09-19 11:51:10 -0400

    Submision info

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 export default function Labs() {
   return (
     <div id="wd-labs">
+      <h2>Submission Info</h2>
+      <p><b>Name:</b> Victoria Postler<br/><b>Section:</b> CS4550 (Online)<br/>
+      <b>GitHub Repo:</b> <a href="https://github.com/tpostler/kambaz-next-js">Link</a>
+      </p>
       <h1>Labs</h1>
       <ul>
         <li>
