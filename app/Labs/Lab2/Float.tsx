fc5e40425d5f39b8356546368ad1054870cb3ea8
Tori <tpostler87@gmail.com>
2025-10-03 12:19:38 -0400

    Lab 2 Done !

---
+++ app/Labs/Lab2/Float.tsx
@@ -0,0 +1,63 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going through
+        the cites of the word in classical literature, discovered the
+        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
+        of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by
+        Cicero, written in 45 BC. This book is a treatise on the theory of
+        ethics,
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        <div className="wd-float-done"></div>
+      </div>
+      <div id="wd-float-divs">
+        <h2>Float</h2>
+        <div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+            Yellow{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+            Blue{" "}
+          </div>
+          <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+            Red{" "}
+          </div>
+          <img
+            className="wd-float-right"
+            src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          />
+          <div className="wd-float-done"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
