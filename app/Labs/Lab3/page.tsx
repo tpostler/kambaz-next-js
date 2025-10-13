2d4553e11bc860447ecc536c1be018d4ee2fe47d
Tori <tpostler87@gmail.com>
2025-10-13 13:19:32 -0400

    Up to 3.6.3

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -23,11 +23,17 @@ import Spreading from "./Spreader";
 import Destructing from "./Destructing";
 import FunctionDestructing from "./FunctionDestructing";
 import DestructingImports from "./DestructingImports";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
 
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
+      <h3>JavaScript</h3>
       <VariablesAndConstants/>
       <VariableTypes/>
       <BooleanVariable/>
@@ -53,5 +59,15 @@ export default function Lab3() {
       <Destructing/>
       <FunctionDestructing/>
       <DestructingImports/>
+      <Classes/>
+      <Styles/>
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr/>
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
     </div>
 );}
--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,25 +1,34 @@
+"use client";
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function TOC() {
- return (
+  const pathname = usePathname();
+
+  return (
    <Nav variant="pills">
      <NavItem>
-       <NavLink href="/Labs" as={Link}>Labs</NavLink>
+       <NavLink href="/Labs" as={Link}  className={`nav-link ${pathname.endsWith("Labs") ? "active" : ""}`}>
+       Labs</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="/Labs/Lab1" as={Link}>Lab 1</NavLink>
+       <NavLink href="/Labs/Lab1" as={Link} className={`nav-link ${pathname.endsWith("Lab1") ? "active" : ""}`}>
+       Lab 1</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="/Labs/Lab2" as={Link}>Lab 2</NavLink>
+       <NavLink href="/Labs/Lab2" as={Link} className={`nav-link ${pathname.endsWith("Lab2") ? "active" : ""}`}>
+       Lab 2</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="/Labs/Lab3" as={Link}>Lab 3</NavLink>
+       <NavLink href="/Labs/Lab3" as={Link} className={`nav-link ${pathname.endsWith("Lab3") ? "active" : ""}`}>
+       Lab 3</NavLink>
      </NavItem>
      <NavItem>
        <NavLink href="/Kambaz" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+       <NavLink href="https://github.com/tpostler">My GitHub</NavLink>
      </NavItem>
    </Nav>
 );}
