44f7ad1b5675310ef65992b6c298a86a7d55c19a
Tori <tpostler87@gmail.com>
2025-10-17 21:08:50 -0400

    Done, but I now have to clean up the jsons and paths :(

--- app/Kambaz/Courses/[cid]/People/Table/page.tsx
+++ app/Kambaz/Courses/[cid]/People/Table/page.tsx
@@ -1,23 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import React from "react";
+import { useParams } from "next/navigation";
+import * as db from "../../../../Database";
+
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
 export default function PeopleTable() {
- return (
-  <div id="wd-people-table">
-   <Table striped>
-    <thead>
-     <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
-    </thead>
-    <tbody>
-     <tr><td className="wd-full-name text-nowrap">
-          <FaUserCircle className="me-2 fs-1 text-secondary" />
-          <span className="wd-first-name">Tony</span>{" "}
-          <span className="wd-last-name">Stark</span></td>
-      <td className="wd-login-id">001234561S</td>
-      <td className="wd-section">S101</td>
-      <td className="wd-role">STUDENT</td>
-      <td className="wd-last-activity">2020-10-01</td>
-      <td className="wd-total-activity">10:21:32</td></tr>
+  const { cid } = useParams();
+  const { users, enrollments } = db;
+  return (
+    <div id="wd-people-table">
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Login ID</th>
+            <th>Section</th>
+            <th>Role</th>
+            <th>Last Activity</th>
+            <th>Total Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users
+            .filter((usr) =>
+              enrollments.some(
+                (enrollment) =>
+                  enrollment.user === usr._id && enrollment.course === cid
+              )
+            )
+            .map((user: any) => (
+              <tr key={user._id}>
+                <td className="wd-full-name text-nowrap">
+                  <FaUserCircle className="me-2 fs-1 text-secondary" />
+                  <span className="wd-first-name">Tony</span>{" "}
+                  <span className="wd-last-name">Stark</span>
+                </td>
+                <td className="wd-login-id">001234561S</td>
+                <td className="wd-section">S101</td>
+                <td className="wd-role">STUDENT</td>
+                <td className="wd-last-activity">2020-10-01</td>
+                <td className="wd-total-activity">10:21:32</td>
+              </tr>
+            ))}
 
+          {/*
       <tr><td className="wd-full-name text-nowrap">
           <FaUserCircle className="me-2 fs-1 text-secondary" />
           <span className="wd-first-name">Star</span>{" "}
@@ -47,7 +75,10 @@ export default function PeopleTable() {
       <td className="wd-role">STUDENT</td>
       <td className="wd-last-activity">2020-12-01</td>
       <td className="wd-total-activity">10:11:32</td></tr>
-          {/* Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
-    </tbody>
-   </Table>
-  </div> );}
+          {/* Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff 
+          */}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
