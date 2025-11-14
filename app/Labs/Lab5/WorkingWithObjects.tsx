6fd99fcc80b8ed5fc5d1165bcf8c98cb03aa325a
Tori <tpostler87@gmail.com>
2025-11-14 11:08:20 -0500

    Lab done

---
+++ app/Labs/Lab5/WorkingWithObjects.tsx
@@ -0,0 +1,136 @@
+"use client";
+import React, { useState } from "react";
+import { Form, FormControl } from "react-bootstrap";
+
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+
+export default function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const [module, setModule] = useState({
+    id: 1,
+    name: "module 1",
+    description: "module description",
+    course: "CS1234",
+  });
+  const ASSIGNMENT_API_URL = `${HTTP_SERVER}/lab5/assignment`;
+  const MODULE_API_URL = `${HTTP_SERVER}/lab5/module`;
+
+  return (
+    <div id="wd-working-with-objects">
+      <h3>Working With Objects</h3> <br />
+      <h4>Assignment Object</h4> <br />
+      <h4>Retrieving Objects</h4>
+      <a
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/assignment`}
+      >
+        Get Assignment
+      </a>
+      <hr />
+      <h4>Retrieving Properties</h4>
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/assignment/title`}
+      >
+        Get Title
+      </a>
+      <hr />
+      {/* Assignment Modifications */}
+      <h4>Modifying Properties</h4>
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/title/${assignment.title}`}
+      >
+        Update Title{" "}
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-title"
+        defaultValue={assignment.title}
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+      />
+      <br />
+      <h4>Modifying Score</h4>
+      <a
+        id="wd-update-assignment-score"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/score/${assignment.score}`}
+      >
+        Update Score{" "}
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-score"
+        type="number"
+        defaultValue={assignment.score}
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: Number(e.target.value) })
+        }
+      />
+      
+      <h4>Toggle Completion</h4>
+      <a
+        id="wd-update-assignment-completed"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/completed/${assignment.completed}`}
+      >
+        Update Completed{" "}
+      </a>
+      <Form.Check
+        className="w-75"
+        id="wd-assignment-completed"
+        type="checkbox"
+        checked={assignment.completed}
+        onChange={(e) =>
+          setAssignment({ ...assignment, completed: e.target.checked })
+        }
+      />
+      <hr />
+      {/* Module Modifications */}
+      <h4>Module Object</h4> <br />
+      <h4>Retrieving Module</h4>
+      <a
+        id="wd-retrieve-module"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/module`} >
+        Get Module
+      </a>
+      <hr />
+      <h4>Get Module Name</h4>
+      <a
+        id="wd-retrieve-module-tnameitle"
+        className="btn btn-primary"
+        href={`${HTTP_SERVER}/lab5/module/name`}
+      >
+        Get Name
+      </a>
+      <hr />
+      <h4>Update Module Name</h4>
+      <a
+        id="wd-update-module-name"
+        className="btn btn-primary float-end"
+        href={`${MODULE_API_URL}/name/${module.name}`}
+      >
+        Update Name{" "}
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-module-name"
+        defaultValue={module.name}
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+      />
+    </div>
+  );
+}
--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -29,6 +29,10 @@ export default function TOC() {
         Lab 4</NavLink>
      </NavItem>
      <NavItem>
+       <NavLink href="/Labs/Lab5" as={Link} className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}>
+        Lab 5</NavLink>
+     </NavItem>
+     <NavItem>
        <NavLink href="/Kambaz" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
